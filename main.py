45ce94a46c292e443a0536f4ffc91f840ebd6e3b
Jessica Temporal <jessicatemporal@gmail.com>
2021-02-27 11:00:03 -0300

    Adds quotes database "mock" with dictionary

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+QUOTES_DB = {
+    "forty_two": "The answer to life the universe and everything",
+    "harry_met_sally": "I'll have what she's having"
+}
 
 @app.get("/")
 def hello():
